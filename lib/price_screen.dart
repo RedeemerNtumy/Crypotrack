484eec17cb8707f4b9deffc7f92c1afd2d14b6f8
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-09-04 21:23:02 +0000

    Showed the currency list

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +7,20 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  List<DropdownMenuItem<String>> getDropDownItems() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (String coin in currenciesList) {
+      var dropDown = DropdownMenuItem(
+        child: Text(coin),
+        value: coin,
+      );
+      dropDownItems.add(dropDown);
+    }
+    return dropDownItems;
+  }
+
+  String selectedCurrency = "USD";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +57,15 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: getDropDownItems(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value.toString();
+                });
+              },
+            ),
           ),
         ],
       ),
