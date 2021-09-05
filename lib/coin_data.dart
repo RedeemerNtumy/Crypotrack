0784fc6df25ed6729a0493d78f25eba38cb65d55
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-09-05 17:37:31 +0000

    Using Coinapi

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,24 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  CoinData(this.url);
+  final String url;
+
+  Future getCoinData() async {
+    http.Response response = await http.get(
+      Uri.parse(url),
+    );
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var rate = jsonDecode(data)["rate"];
+
+      return rate;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
+
+const bitcoinUrl =
+    "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DEC3C8C2-EACC-4139-A140-44D09C9C3A66";
