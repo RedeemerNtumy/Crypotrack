18d2c442a83fb954b49203f56380b959a6c8e948
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-09-05 18:47:57 +0000

    Eating

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -57,6 +57,12 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    getRate();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -77,7 +83,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  "",
+                  "1 BTC=$bitcoinUSD USD",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
